e3deaf54f711f2b8e13fb9703673b0aceafdd411
Yusuf Shakhpaz <shakhpazyusuf@gmail.com>
2026-01-19 22:44:22 -0800

    displaying tung tung sahur with healthbar

--- main.js
+++ main.js
@@ -2,11 +2,16 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+const grid = new Grid();
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 
+	const t = new Tungtungsahur(10, 10, 0, 0)
 	gameEngine.init(ctx);
+
+	gameEngine.addEntity(t)
 
 	gameEngine.start();
 });
