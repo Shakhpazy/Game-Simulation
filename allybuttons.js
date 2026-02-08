51a717c4feb36b083d13279fc8a93ddbaac8eda2
KhalidM23 <khalidmoham2324@gmail.com>
2026-02-08 10:40:30 -0800

    Added images and highlight yellow when selected for allys and shovel

--- allybuttons.js
+++ allybuttons.js
@@ -2,40 +2,44 @@ class allyButtons {
 
     constructor(gameEngine) {
         this.gameEngine = gameEngine;
-        this.shovel = new AllyButton(gameEngine, "Shovel", 0, 315, 0, (button) => {
+		this.allyoneBgImage = ASSET_MANAGER.getAsset('./Sprites/goku.png')
+		this.allytwoBgImage = ASSET_MANAGER.getAsset('./Sprites/ichigo.png')
+		this.allythreeBgImage = ASSET_MANAGER.getAsset('./Sprites/naruto.png')
+		this.shovelBgImage = ASSET_MANAGER.getAsset('./Sprites/shovel.png')
+        this.shovel = new AllyButton(gameEngine, "Shovel", 0, 420, 0, (button) => {
         	console.log("shovel 1 button clicked");
         	gameEngine.towerManager.selectedTowerType = button.selected ? null : 'Shovel'; 
         	button.gameEngine.selectedButton = button;
         	button.gameEngine.click = null;
         	console.log("Selected Tower Type:", gameEngine.towerManager.selectedTowerType);
-        });
+        }, this.shovelBgImage);
 
-        this.allyone = new AllyButton(gameEngine, "Goku", 15, 0, 0, (button) => {
+        this.allyone = new AllyButton(gameEngine, "Goku", 15, 300, 0, (button) => {
         	console.log("ally 1 button clicked");
         	gameEngine.towerManager.selectedTowerType = button.selected ? null : 'Goku'; 
         	button.gameEngine.selectedButton = button;
         	button.gameEngine.click = null;
         	console.log("Selected Tower Type:", gameEngine.towerManager.selectedTowerType);
         	// Money check happens later when placing on grid, not here
-        });
-
-        this.allytwo = new AllyButton(gameEngine, "Ichigo", 5, 105, 0,(button) => {
+        }, this.allyoneBgImage);
+        this.allytwo = new AllyButton(gameEngine, "Ichigo", 5, 60, 0,(button) => {
         	console.log("ally 2 button clicked");
         	gameEngine.towerManager.selectedTowerType = button.selected ? null : 'Ichigo'; 
         	button.gameEngine.selectedButton = button;
         	button.gameEngine.click = null;
         	console.log("Selected Tower Type:", gameEngine.towerManager.selectedTowerType);
         	// Money check happens later when placing on grid, not here
-        });
+        }, this.allytwoBgImage);
         
-        this.allythree = new AllyButton(gameEngine, "Naruto", 10, 210, 0, (button) => {
+        this.allythree = new AllyButton(gameEngine, "Naruto", 10, 180, 0, (button) => {
         	console.log("ally 3 button clicked");
         	gameEngine.towerManager.selectedTowerType = button.selected ? null : 'Naruto'; 
         	button.gameEngine.selectedButton = button;
         	button.gameEngine.click = null;
         	console.log("Selected Tower Type:", gameEngine.towerManager.selectedTowerType);
         	// Money check happens later when placing on grid, not here
-        });
+        }, this.allythreeBgImage);
+		
 
         gameEngine.addEntity(this.allyone);
         gameEngine.addEntity(this.allytwo);
